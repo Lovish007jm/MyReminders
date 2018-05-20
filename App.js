21a52249917cd739356cc0ec45804c1a6d994c61
Lovish <lovish007jm@gmail.com>
2018-05-20 23:11:31 +0530

    MyReminder repository created
    Created a react-native project jus added a header component,
    Currently everything works fine

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import TopHeader from './components/TopHeader';
+
+const App = ()=>{
+ return(
+    <TopHeader/>
+  );
+}
+
+
+
+export default App;
---
+++ components/TopHeader.js
@@ -0,0 +1,25 @@
+import  React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+class TopHeader extends Component{
+
+render(){
+  const { headerStyle } = styles;
+      return(
+        <View style={ headerStyle }>
+          <Text> Title </Text>
+        </View>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  headerStyle:{
+    height:50,
+    backgroundColor:'#EEEEEE',
+    color: '#EEEEEE',
+    elevation:2,
+  },
+});
+
+export default TopHeader;
