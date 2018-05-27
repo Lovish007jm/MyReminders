fb8260e0998cc9efe502ec6ca48327aff314e474
Manoj Pathak <manoj.pathak1215@gmail.com>
2018-05-27 09:19:14 +0530

    FEATURE-1001: Passing isSearch props from the parent component

--- components/HeaderCenterComp.js
+++ components/HeaderCenterComp.js
@@ -1,46 +1,41 @@
-import React,{ Component } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity,TextInput } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native';
 
-class HeaderCenterComp extends Component{
-    constructor(props){
+class HeaderCenterComp extends Component {
+    constructor(props, context) {
         super(props);
-        this.state = { textFieldEnable : 1 }
-
     }
 
-    render(){
-        const { titleStyle,container } = styles;
-        var element = this.state.textFieldEnable ? <TextInput  style = {titleStyle} placeholder="Type here to search!"/>:<Text style={ titleStyle }>Reminder</Text>
-        return(
-            <View style = {container}>
+    render() {
+        const { titleStyle, container } = styles;
+        const { isSearch } = this.props;
+        const element = isSearch ? <TextInput style={titleStyle} placeholder="Type here to search!" /> : <Text style={titleStyle}>Reminder</Text>
+        return (
+            <View style={container}>
                 {element}
-             
-           
-           
             </View>
         );
 
     }
 }
 
-
 const styles = StyleSheet.create({
-    titleStyle:{
-        color:'#FFFFFF',
-        fontSize:15,
-        flex:1
-        
+    titleStyle: {
+        color: '#FFFFFF',
+        fontSize: 15,
+        flex: 1
+
     },
-    container:{
-        borderWidth:2,
-        borderColor:'#FFFFFF',
-        flex:2,
-        paddingLeft:8,
-        paddingRight:8,
-        flexDirection:'row',
-        alignItems:'center',
-        marginLeft:3
+    container: {
+        borderWidth: 2,
+        borderColor: '#FFFFFF',
+        flex: 2,
+        paddingLeft: 8,
+        paddingRight: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 3
     },
-    
 });
+
 export default HeaderCenterComp;
--- components/TopHeader.js
+++ components/TopHeader.js
@@ -1,40 +1,55 @@
-import  React, { Component } from 'react';
-import { Text, View, StyleSheet,Image,TouchableOpacity } from 'react-native';
-import  HeaderButton  from './HeaderButton';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import HeaderButton from './HeaderButton';
 import HeaderCenterComp from './HeaderCenterComp';
-class TopHeader extends Component{
 
-render(){
-  const { headerStyle,searchBtnStyle } = styles;
-      return(
+class TopHeader extends Component {
 
-        <View style={ headerStyle }>
-          
-          <HeaderButton/>
-          <HeaderCenterComp/>
-          <TouchableOpacity>
-                <Image style ={ searchBtnStyle} source={require('../images/search.png')}/>
-            </TouchableOpacity>
-        </View>
-      );
+    constructor(props, context) {
+        this.state = {
+            showSearch: false,
+        };
+        this.onClickSearch = this.onClickSearch.bind(this);
+    }
+
+    onClickSearch() {
+        const { showSearch } = this.state;
+        this.setState({ showSearch: !showSearch });
+    }
+
+    render() {
+        const { headerStyle, searchBtnStyle } = styles;
+        const { onClickSearch } = this;
+        const { showSearch } = this.state;
+
+        return (
+
+            <View style={headerStyle}>
+                <HeaderButton isSearch={showSearch} />
+                <HeaderCenterComp isSearch={showSearch} />
+                <TouchableOpacity onClick={onClickSearch}>
+                    <Image style={searchBtnStyle} source={require('../images/search.png')} />
+                </TouchableOpacity>
+            </View>
+        );
     }
 }
 
 const styles = StyleSheet.create({
-  headerStyle:{
-    height:45,
-    backgroundColor:'#252728',
-    elevation:2,
-    padding:5,
-    flexDirection:'row',
-    justifyContent:'flex-start',
-    alignItems:'center',
-  
-  },
-  searchBtnStyle:{
-    width:25,
-    height:25
-}
+    headerStyle: {
+        height: 45,
+        backgroundColor: '#252728',
+        elevation: 2,
+        padding: 5,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+
+    },
+    searchBtnStyle: {
+        width: 25,
+        height: 25
+    }
 });
 
 export default TopHeader;
